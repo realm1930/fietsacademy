6a639936dd544e66a42a6c30a95bad870b373c68
Arne <a.vaneycken@gmail.com>
2020-11-27 15:20:45 +0100

    Verzameling Value objecten, begin

--- src/main/java/be/vdab/fietsacademy/domain/Docent.java
+++ src/main/java/be/vdab/fietsacademy/domain/Docent.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "docenten")
@@ -18,6 +21,11 @@ public class Docent {
     private String emailAdres;
     @Enumerated(EnumType.STRING)
     private Geslacht geslacht;
+    @ElementCollection
+    @CollectionTable(name = "docentenbijnamen",
+            joinColumns = @JoinColumn(name = "docentid") )
+    @Column(name = "bijnaam")
+    private Set<String> bijnamen;
 
     public Docent(String voornaam, String familienaam, BigDecimal wedde,
                   String emailAdres, Geslacht geslacht) {
@@ -26,6 +34,8 @@ public class Docent {
         this.wedde = wedde;
         this.emailAdres = emailAdres;
         this.geslacht = geslacht;
+        this.bijnamen = new LinkedHashSet<>();
+
     }
 
     protected Docent(){}
@@ -60,5 +70,17 @@ public class Docent {
         }
         var factor = BigDecimal.ONE.add(percentage.divide(BigDecimal.valueOf(100)));
         wedde = wedde.multiply(factor).setScale(2, RoundingMode.HALF_UP);
+    }
+    public Set<String> getBijnamen() {
+        return Collections.unmodifiableSet(bijnamen);
+    }
+    public boolean addBijnaam(String bijnaam) {
+        if (bijnaam.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        return bijnamen.add(bijnaam);
+    }
+    public boolean removeBijnaam(String bijnaam) {
+        return bijnamen.remove(bijnaam);
     }
 }
