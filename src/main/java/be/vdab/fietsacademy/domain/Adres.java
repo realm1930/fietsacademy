a9bbf6114730daed03751cb2d743cc48aee6ab54
Arne <a.vaneycken@gmail.com>
2020-11-27 14:53:01 +0100

    Begin VALUE OBJECTS

---
+++ src/main/java/be/vdab/fietsacademy/domain/Adres.java
@@ -0,0 +1,40 @@
+package be.vdab.fietsacademy.domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Access(AccessType.FIELD)
+public class Adres {
+    private String straat;
+    private String huisNr;
+    private String postcode;
+    private String gemeente;
+
+    public Adres(String straat, String huisNr, String postcode, String gemeente) {
+        this.straat = straat;
+        this.huisNr = huisNr;
+        this.postcode = postcode;
+        this.gemeente = gemeente;
+    }
+
+    protected Adres() {
+    }
+
+    public String getStraat() {
+        return straat;
+    }
+
+    public String getHuisNr() {
+        return huisNr;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public String getGemeente() {
+        return gemeente;
+    }
+}
---
+++ src/main/java/be/vdab/fietsacademy/domain/Campus.java
@@ -0,0 +1,20 @@
+package be.vdab.fietsacademy.domain;
+
+import javax.persistence.*;
+
+@Entity @Table(name = "campussen")
+public class Campus {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String naam;
+    @Embedded
+    private Adres adres;
+
+    public Campus(String naam, Adres adres) {
+        this.naam = naam;
+        this.adres = adres;
+    }
+
+    protected Campus() {
+    }
+}
